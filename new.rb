e8fc30b6f1c2245500bdd392d7a72b67073e153f
ChiakiTakayama <106967678+ChiakiTakayama@users.noreply.github.com>
2022-06-18 08:48:32 +0000

    [update]手直し済み

--- new.rb
+++ new.rb
@@ -1,11 +1,14 @@
 class Car
   def run(distance)
     puts "車で#{distance}キロ走ります。"
-  else
-  def turn_(right)
-    puts"車で#{right}に曲がります。"
+  end
+  def turn(direction)
+    puts"車で#{direction}に曲がります。"
+ end
 end
+
 car = Car.new
 car.run(5)
+
 car = Car.new
 car.turn("右")
