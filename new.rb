fcf958bf2dc369588d6e61afa9be25722abcf547
ChiakiTakayama <106967678+ChiakiTakayama@users.noreply.github.com>
2022-06-18 03:49:00 +0000

    [update]Ruby9章

--- new.rb
+++ new.rb
@@ -1,7 +1,11 @@
 class Car
   def run(distance)
     puts "車で#{distance}キロ走ります。"
-  end
+  else
+  def turn_(right)
+    puts"車で#{right}に曲がります。"
 end
 car = Car.new
 car.run(5)
+car = Car.new
+car.turn("右")
